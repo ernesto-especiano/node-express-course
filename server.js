60991aeccc78d2ac06191b6f773dc5abb5df1f62
Ernesto Especiano <eespeciano@unitedvirtualities.com>
2020-02-17 18:03:49 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app= express();
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -24,6 +26,27 @@ app.get('/users/:id',function(req,res){
     })
 })
 
+app.post('/login',function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if(username==mockUsername && password==mockPassword){
+        res.json({
+            success:true,
+            message:'password and username match!',
+            token: 'encrypted token goes here'
+
+        })
+    }else {
+        res.json({
+            success:false,
+            message:'password or username do not match'
+        })
+    }
+})
 
 
 app.listen(8000,function(){
